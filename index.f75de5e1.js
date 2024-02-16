6673baf3870182b530b5030d6424fd1512ccc5c8
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-02-16 09:16:23 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,36 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const message = document.createElement("div");
+    const messageTitle = document.createElement("h2");
+    const messageP = document.createElement("p");
+    message.classList.add("notification");
+    messageTitle.classList.add("title");
+    message.appendChild(messageTitle);
+    message.appendChild(messageP);
+    message.style.top = posTop + "px";
+    message.style.right = posRight + "px";
+    messageTitle.textContent = title;
+    messageP.textContent = description;
+    switch(type){
+        case "warning":
+            message.classList.add("warning");
+            break;
+        case "success":
+            message.classList.add("success");
+            break;
+        case "error":
+            message.classList.add("error");
+            break;
+        default:
+            break;
+    }
+    document.body.appendChild(message);
+    setTimeout(()=>{
+        message.style.visibility = "hidden";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
